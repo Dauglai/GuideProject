ac78fc6a314ce0d095909743ac0c3dd7cbc823b1
Dauglai <danil.samartsev.04@gmail.com>
2023-07-07 21:50:59 +0500

    models

--- GuideProject/urls.py
+++ GuideProject/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from reviews.views import ReviewAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/reviewlist/', ReviewAPIView.as_view())
 ]
