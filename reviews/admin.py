508a0b6b00a4810efec53c8e3fd6581dff48b9c4
Dauglai <danil.samartsev.04@gmail.com>
2023-07-13 14:47:23 +0500

    подключение vk api группы CС

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Course, Teacher, Review
-# Register your models here.
+from .models import Course, Teacher, Review, Discipline, FormEducation
 admin.site.register(Review)
 admin.site.register(Teacher)
 admin.site.register(Course)
+admin.site.register(Discipline)
+admin.site.register(FormEducation)
